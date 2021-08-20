d376bd4923d1d57efd955df875ada9fe0a4f88c6
TaegyunWoo <dnxprbs@gmail.com>
2021-08-20 18:57:13 +0900

    Add Basic DB

---
+++ src/main/java/com/my/springboot/domain/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.my.springboot.domain.posts;
+
+import com.my.springboot.web.dto.PostsReponseDto;
+import com.my.springboot.web.dto.PostsSaveRequestDto;
+import com.my.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsReponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsReponseDto(entity);
+
+    }
+}
