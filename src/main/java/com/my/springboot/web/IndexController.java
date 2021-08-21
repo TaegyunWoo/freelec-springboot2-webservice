18fd4413ea32b6f32fafcce4d530a49388f461e4
TaegyunWoo <dnxprbs@gmail.com>
2021-08-21 16:21:31 +0900

    Add Complete Google Login Func

--- src/main/java/com/my/springboot/web/IndexController.java
+++ src/main/java/com/my/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.my.springboot.web;
 
+import com.my.springboot.config.auth.LoginUser;
+import com.my.springboot.config.auth.SessionUser;
 import com.my.springboot.domain.posts.PostsService;
 import com.my.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
@@ -32,4 +42,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
